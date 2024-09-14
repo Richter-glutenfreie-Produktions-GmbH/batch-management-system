9ecf83a0328195df34c6ba191c1d91b959b00737
trueberryless <99918022+trueberryless@users.noreply.github.com>
2024-09-14 08:09:36 +0000

    make codespace server actions possible

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            allowedOrigins: [
+                "sturdy-garbanzo-p5w9pqg5pgg379q-3000.app.github.dev",
+                "localhost:3000",
+            ],
+        },
+    },
+};
 
 module.exports = nextConfig;

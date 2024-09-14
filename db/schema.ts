ed92e747c3553705adef399547e3689deca37971
trueberryless <trueberryless+github@gmail.com>
2024-09-14 17:51:25 +0200

    foreign keys

--- db/schema.ts
+++ db/schema.ts
@@ -9,20 +9,44 @@ import {
     primaryKey,
     unique,
     uuid,
+    AnyPgColumn,
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
-export const productBatches = pgTable("product_batches", {
+export const productBatch = pgTable("product_batches", {
     id: uuid("id").notNull().primaryKey().defaultRandom(),
-    productId: uuid("product_id").notNull(),
-    batchId: uuid("batch_id").notNull(),
-    recipeId: uuid("recipe_id").notNull(),
+    productId: uuid("product_id")
+        .notNull()
+        .references(() => product.id, { onDelete: "cascade" }),
+    batchId: uuid("batch_id")
+        .notNull()
+        .references(() => batch.id, { onDelete: "cascade" }),
+    recipeId: uuid("recipe_id")
+        .notNull()
+        .references(() => recipe.id, { onDelete: "cascade" }),
 });
 
-export const recipes = pgTable("recipes", {
+export const productBatchRelations = relations(productBatch, ({ one }) => ({
+    product: one(product, {
+        fields: [productBatch.productId],
+        references: [product.id],
+    }),
+    batch: one(batch, {
+        fields: [productBatch.batchId],
+        references: [batch.id],
+    }),
+    recipe: one(recipe, {
+        fields: [productBatch.recipeId],
+        references: [recipe.id],
+    }),
+}));
+
+export const recipe = pgTable("recipes", {
     id: uuid("id").notNull().primaryKey().defaultRandom(),
     name: text("name"),
-    productId: uuid("product_id").notNull(),
+    productId: uuid("product_id")
+        .notNull()
+        .references(() => product.id, { onDelete: "cascade" }),
     isObsolete: boolean("is_obsolete").notNull(),
     insertedAt: timestamp("inserted_at", { mode: "date", precision: 3, withTimezone: false })
         .notNull()
@@ -32,7 +56,16 @@ export const recipes = pgTable("recipes", {
         .$onUpdate(() => new Date()),
 });
 
-export const configurations = pgTable("configurations", {
+export const recipeRelations = relations(recipe, ({ one, many }) => ({
+    product: one(product, {
+        fields: [recipe.productId],
+        references: [product.id],
+    }),
+    productBatches: many(productBatch),
+    recipeHasIngredients: many(recipeHasIngredient),
+}));
+
+export const configuration = pgTable("configurations", {
     id: uuid("id").notNull().primaryKey().defaultRandom(),
     version: integer("version").notNull(),
     insertedAt: timestamp("inserted_at", { mode: "date", precision: 3, withTimezone: false })
@@ -43,23 +76,34 @@ export const configurations = pgTable("configurations", {
         .$onUpdate(() => new Date()),
 });
 
-export const products = pgTable("products", {
-    id: uuid("id").notNull().primaryKey().defaultRandom(),
-    currentRecipeId: uuid("current_recipe_id"),
+export const product = pgTable("products", {
+    id: uuid("id").notNull().primaryKey().defaultRandom(), // todo: is from base table ingredient
+    currentRecipeId: uuid("current_recipe_id").references((): AnyPgColumn => recipe.id),
     massValue: doublePrecision("mass_value").notNull(),
     massUnitId: uuid("mass_unit_id").notNull(),
     isActive: boolean("is_active").notNull(),
 });
 
-export const customers = pgTable("customers", {
+export const productRelations = relations(product, ({ one, many }) => ({
+    recipes: many(recipe),
+    currentRecipe: one(recipe, {
+        fields: [product.currentRecipeId],
+        references: [recipe.id],
+    }),
+    productBatches: many(productBatch),
+}));
+
+export const customer = pgTable("customers", {
     id: uuid("id").notNull().primaryKey().defaultRandom(),
     name: text("name").notNull(),
     description: text("description"),
 });
 
-export const rawMaterials = pgTable("raw_materials", {
+export const rawMaterial = pgTable("raw_materials", {
     supplyItemId: uuid("supply_item_id").notNull().primaryKey(),
-    receivedBatchesId: uuid("received_batches_id").notNull(),
+    receivedBatchesId: uuid("received_batches_id")
+        .notNull()
+        .references(() => receivedBatch.id, { onDelete: "cascade" }),
     number: text("number").notNull().unique(),
     overrideIsVegan: boolean("override_is_vegan"),
     overrideIsVegetarian: boolean("override_is_vegetarian"),
@@ -73,7 +117,18 @@ export const rawMaterials = pgTable("raw_materials", {
         .$onUpdate(() => new Date()),
 });
 
-export const ingredientsBt = pgTable("ingredients_bt", {
+export const rawMaterialRelations = relations(rawMaterial, ({ one }) => ({
+    supplyItem: one(supplyItem, {
+        fields: [rawMaterial.supplyItemId],
+        references: [supplyItem.id],
+    }),
+    receivedBatch: one(receivedBatch, {
+        fields: [rawMaterial.receivedBatchesId],
+        references: [receivedBatch.id],
+    }),
+}));
+
+export const ingredient = pgTable("ingredients_bt", {
     id: uuid("id").notNull().primaryKey().defaultRandom(),
     number: text("number").notNull().unique(),
     name: text("name").notNull(),
@@ -86,18 +141,24 @@ export const ingredientsBt = pgTable("ingredients_bt", {
         .$onUpdate(() => new Date()),
 });
 
-export const manufacturedBatches = pgTable("manufactured_batches", {
-    id: uuid("id").notNull().primaryKey().defaultRandom(),
+export const ingredientRelations = relations(ingredient, ({ many }) => ({
+    recipeHasIngredients: many(recipeHasIngredient),
+}));
+
+export const manufacturedBatch = pgTable("manufactured_batches", {
+    id: uuid("id").notNull().primaryKey().defaultRandom(), // todo: is from base table batch
     manufacturedOn: date("manufactured_on").notNull(),
 });
 
-export const batchesBt = pgTable("batches_bt", {
+export const batch = pgTable("batches_bt", {
     id: uuid("id").notNull().primaryKey().defaultRandom(),
     number: text("number").notNull(),
     expiresOn: date("expires_on").notNull(),
     note: text("note"),
     massValue: doublePrecision("mass_value").notNull(),
-    massUnitId: uuid("mass_unit_id").notNull(),
+    massUnitId: uuid("mass_unit_id")
+        .notNull()
+        .references(() => unit.id, { onDelete: "cascade" }),
     insertedAt: timestamp("inserted_at", { mode: "date", precision: 3, withTimezone: false })
         .notNull()
         .defaultNow(),
@@ -106,12 +167,24 @@ export const batchesBt = pgTable("batches_bt", {
         .$onUpdate(() => new Date()),
 });
 
-export const receivedBatches = pgTable("received_batches", {
-    id: uuid("id").notNull().primaryKey().defaultRandom(),
+export const batchRelations = relations(batch, ({ one, many }) => ({
+    productBatches: many(productBatch),
+    massUnit: one(unit, {
+        fields: [batch.massUnitId],
+        references: [unit.id],
+    }),
+}));
+
+export const receivedBatch = pgTable("received_batches", {
+    id: uuid("id").notNull().primaryKey().defaultRandom(), // todo: is from base table batch
     deliveredOn: date("delivered_on").notNull(),
 });
 
-export const units = pgTable("units", {
+export const receivedBatchRelations = relations(receivedBatch, ({ many }) => ({
+    rawMaterials: many(rawMaterial),
+}));
+
+export const unit = pgTable("units", {
     id: uuid("id").notNull().primaryKey().defaultRandom(),
     symbol: text("symbol").notNull(),
     label: text("label").notNull(),
@@ -123,17 +196,30 @@ export const units = pgTable("units", {
         .$onUpdate(() => new Date()),
 });
 
-export const recipeHasIngredientsJt = pgTable(
+export const unitRelations = relations(unit, ({ many }) => ({
+    fromUnits: many(unitConversion, { relationName: "fromUnit" }),
+    toUnits: many(unitConversion, { relationName: "toUnit" }),
+    recipeHasIngredients: many(recipeHasIngredient),
+    batches: many(batch),
+}));
+
+export const recipeHasIngredient = pgTable(
     "recipe_has_ingredients_jt",
     {
-        ingredientId: uuid("ingredient_id").notNull(),
-        recipeId: uuid("recipe_id").notNull(),
+        ingredientId: uuid("ingredient_id")
+            .notNull()
+            .references(() => ingredient.id, { onDelete: "cascade" }),
+        recipeId: uuid("recipe_id")
+            .notNull()
+            .references(() => recipe.id, { onDelete: "cascade" }),
         amountValue: integer("amount_value").notNull(),
-        amountUnitId: uuid("amount_unit_id").notNull(),
+        amountUnitId: uuid("amount_unit_id")
+            .notNull()
+            .references(() => unit.id, { onDelete: "cascade" }),
         insertedAt: timestamp("inserted_at", { mode: "date", precision: 3, withTimezone: false })
             .notNull()
             .defaultNow(),
-        updatedAt: timestamp("updated_at", { mode: "date", precision: 3 })
+        updatedAt: timestamp("updated_at", { mode: "date", precision: 3, withTimezone: false })
             .notNull()
             .$onUpdate(() => new Date()),
     },
@@ -142,25 +228,50 @@ export const recipeHasIngredientsJt = pgTable(
     })
 );
 
-export const supplyItem = pgTable("supply_item", {
-    id: uuid("id").notNull().primaryKey().defaultRandom(),
+export const recipeHasIngredientRelations = relations(recipeHasIngredient, ({ one }) => ({
+    ingredient: one(ingredient, {
+        fields: [recipeHasIngredient.ingredientId],
+        references: [ingredient.id],
+    }),
+    recipe: one(recipe, {
+        fields: [recipeHasIngredient.recipeId],
+        references: [recipe.id],
+    }),
+    amountUnit: one(unit, {
+        fields: [recipeHasIngredient.amountUnitId],
+        references: [unit.id],
+    }),
+}));
+
+export const supplyItem = pgTable("supply_items", {
+    id: uuid("id").notNull().primaryKey().defaultRandom(), // todo: is from base table ingredient
     isVegan: boolean("is_vegan").notNull().default(false),
     isVegetarian: boolean("is_vegetarian").notNull().default(false),
     isTurkishHalal: boolean("is_turkish_halal").notNull().default(true),
     isJewishKosher: boolean("is_jewish_kosher").notNull().default(true),
 });
 
-export const unitConversions = pgTable(
+export const supplyItemRelations = relations(supplyItem, ({ many }) => ({
+    rawMaterials: many(rawMaterial),
+    // ingredients: many(ingredient),
+    // rawMaterials: many(rawMaterial),
+}));
+
+export const unitConversion = pgTable(
     "unit_conversions",
     {
-        fromUnitId: uuid("from_unit_id").notNull(),
-        toUnitId: uuid("to_unit_id").notNull(),
+        fromUnitId: uuid("from_unit_id")
+            .notNull()
+            .references(() => unit.id, { onDelete: "cascade" }),
+        toUnitId: uuid("to_unit_id")
+            .notNull()
+            .references(() => unit.id, { onDelete: "cascade" }),
         conversionFactor: doublePrecision("conversion_factor").notNull(),
         description: text("description"),
         insertedAt: timestamp("inserted_at", { mode: "date", precision: 3, withTimezone: false })
             .notNull()
             .defaultNow(),
-        updatedAt: timestamp("updated_at", { mode: "date", precision: 3 })
+        updatedAt: timestamp("updated_at", { mode: "date", precision: 3, withTimezone: false })
             .notNull()
             .$onUpdate(() => new Date()),
     },
@@ -169,53 +280,15 @@ export const unitConversions = pgTable(
     })
 );
 
-// Add foreign key relations
-export const relationsSetup = relations(
-    [
-        recipes,
-        productBatches,
-        rawMaterials,
-        ingredientsBt,
-        manufacturedBatches,
-        batchesBt,
-        receivedBatches,
-        units,
-        recipeHasIngredientsJt,
-        supplyItem,
-        unitConversions,
-    ],
-    (table) => ({
-        recipesRelations: {
-            product: table.foreignKey(recipes.productId).references(products.id),
-            currentRecipe: table.foreignKey(products.currentRecipeId).references(recipes.id),
-        },
-        productBatchesRelations: {
-            product: table.foreignKey(productBatches.productId).references(products.id),
-            batch: table.foreignKey(productBatches.batchId).references(manufacturedBatches.id),
-            recipe: table.foreignKey(productBatches.recipeId).references(recipes.id),
-        },
-        rawMaterialsRelations: {
-            supplyItem: table.foreignKey(rawMaterials.supplyItemId).references(supplyItem.id),
-            receivedBatch: table
-                .foreignKey(rawMaterials.receivedBatchesId)
-                .references(receivedBatches.id),
-        },
-        recipeHasIngredientsJtRelations: {
-            ingredient: table
-                .foreignKey(recipeHasIngredientsJt.ingredientId)
-                .references(ingredientsBt.id),
-            recipe: table.foreignKey(recipeHasIngredientsJt.recipeId).references(recipes.id),
-            unit: table.foreignKey(recipeHasIngredientsJt.amountUnitId).references(units.id),
-        },
-        manufacturedBatchesRelations: {
-            batch: table.foreignKey(manufacturedBatches.id).references(batchesBt.id),
-        },
-        batchesBtRelations: {
-            unit: table.foreignKey(batchesBt.massUnitId).references(units.id),
-        },
-        unitConversionsRelations: {
-            fromUnit: table.foreignKey(unitConversions.fromUnitId).references(units.id),
-            toUnit: table.foreignKey(unitConversions.toUnitId).references(units.id),
-        },
-    })
-);
+export const unitConversionRelations = relations(unitConversion, ({ one }) => ({
+    fromUnit: one(unit, {
+        fields: [unitConversion.fromUnitId],
+        references: [unit.id],
+        relationName: "fromUnit",
+    }),
+    toUnit: one(unit, {
+        fields: [unitConversion.toUnitId],
+        references: [unit.id],
+        relationName: "toUnit",
+    }),
+}));
